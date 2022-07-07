49356e2108069129163c00c059f6e179d7ffe4aa
Anton Elmanov <ant.elmanov@gmail.com>
2022-07-07 10:33:36 +0300

    no migrations

--- cmd/swagger/main.go
+++ cmd/swagger/main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"context"
 
-	"github.com/golang-migrate/migrate/v4"
 	"go.uber.org/zap"
 
 	"git.epam.com/epm-lstr/epm-lstr-lc/be/internal/config"
 	"git.epam.com/epm-lstr/epm-lstr-lc/be/internal/db"
 	"git.epam.com/epm-lstr/epm-lstr-lc/be/internal/logger"
-	"git.epam.com/epm-lstr/epm-lstr-lc/be/internal/migration"
 	"git.epam.com/epm-lstr/epm-lstr-lc/be/swagger"
 	"git.epam.com/epm-lstr/epm-lstr-lc/be/swagger/generated/restapi"
 )
@@ -27,11 +25,12 @@ func main() {
 		logger.Fatal("cant open db", zap.Error(err))
 	}
 
-	err = migration.Apply(ctx, sqlDB, entClient)
-	if err != migrate.ErrNoChange && err != nil {
-		logger.Fatal("migration failed", zap.Error(err))
-	}
-	logger.Error("migration error", zap.Error(err))
+	// DO NOT uncomment
+	//err = migration.Apply(ctx, sqlDB, entClient)
+	//if err != migrate.ErrNoChange && err != nil {
+	//	logger.Fatal("migration failed", zap.Error(err))
+	//}
+	//logger.Error("migration error", zap.Error(err))
 
 	// conf
 	serverConfig, err := config.SetupServerConfig()
