0db0b315e388dd2f897d33a34cf0e5412d1d2fc0
Aleksandra Bogoslavets <aleksandra_bogoslavets@epam.com>
2022-09-06 09:53:20 +0000

    Integration tests part 4

--- swagger/integration-tests/photos/integration_test.go
+++ swagger/integration-tests/photos/integration_test.go
@@ -1,26 +1,55 @@
 package photos
 
 import (
-	"bytes"
 	"context"
-	"image"
-	"image/jpeg"
+	"errors"
+	"flag"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"testing"
 
-	"golang.org/x/sys/unix"
-
+	"github.com/go-openapi/runtime"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"git.epam.com/epm-lstr/epm-lstr-lc/be/client/photos"
-	utils "git.epam.com/epm-lstr/epm-lstr-lc/be/swagger/integration-tests/common"
+	"git.epam.com/epm-lstr/epm-lstr-lc/be/swagger/generated/models"
+	"git.epam.com/epm-lstr/epm-lstr-lc/be/swagger/integration-tests/common"
 )
 
 var (
 	testLogin    string
 	testPassword string
+	auth         runtime.ClientAuthInfoWriterFunc
 )
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	if !testing.Short() {
+		ctx := context.Background()
+		beClient := common.SetupClient()
+
+		var err error
+		testLogin, testPassword, err = common.GenerateLoginAndPassword()
+		if err != nil {
+			log.Fatalf("GenerateLoginAndPassword: %v", err)
+		}
+		_, err = common.CreateUser(ctx, beClient, testLogin, testPassword)
+		if err != nil {
+			log.Fatalf("CreateUser: %v", err)
+		}
+		loginUser, err := common.LoginUser(ctx, beClient, testLogin, testPassword)
+		if err != nil {
+			log.Fatalf("LoginUser: %v", err)
+		}
+
+		auth = common.AuthInfoFunc(loginUser.GetPayload().AccessToken)
+
+		os.Exit(m.Run())
+	}
+}
 
 func TestIntegration_PhotosUpload(t *testing.T) {
 	if testing.Short() {
@@ -28,79 +57,306 @@ func TestIntegration_PhotosUpload(t *testing.T) {
 	}
 
 	ctx := context.Background()
-	beClient := utils.SetupClient()
+	beClient := common.SetupClient()
 
-	var err error
-	testLogin, testPassword, err = utils.GenerateLoginAndPassword()
-	if err != nil {
-		log.Fatalf("GenerateLoginAndPassword: %v", err)
-	}
-	_, err = utils.CreateUser(ctx, beClient, testLogin, testPassword)
-	if err != nil {
-		log.Fatalf("CreateUser: %v", err)
-	}
-	loginUser, err := utils.LoginUser(ctx, beClient, testLogin, testPassword)
-	require.NoError(t, err)
-
-	t.Run("", func(t *testing.T) {
-		token := loginUser.GetPayload().AccessToken
-		fileName := "testfile.txt"
-		//os.Mkdir("test", 0600)
-		//id := "testimagename"
-		//url := "http://localhost:8080/api/equipments/photos/testimagename"
-		//fileName := "testimagename.jpg"
-
-		img, err := generateImageBytes()
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = createNonEmptyFile(fileName, img)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer os.Remove(fileName)
-
+	t.Run("Create New Photo Ok JPEG", func(t *testing.T) {
+		fileName := "../common/cat.jpeg"
 		f, err := os.Open(fileName)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-
-		params := photos.NewCreateNewPhotoParamsWithContext(ctx)
-		params.File = f
-		res, err := beClient.Photos.CreateNewPhoto(params, utils.AuthInfoFunc(token))
-		_ = res
 		require.NoError(t, err)
-		// todo: refactor
+
+		params := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+		res, err := beClient.Photos.CreateNewPhoto(params, auth)
+		require.NoError(t, err)
+
+		require.NotNil(t, res.Payload.Data)
+		assert.NotEmpty(t, res.Payload.Data.ID)
+		assert.NotEmpty(t, res.Payload.Data.URL)
+		assert.NotEmpty(t, res.Payload.Data.FileName)
+
+		// cleanup
+		_, err = beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth)
+		require.NoError(t, err)
+		f.Close()
+	})
+
+	t.Run("Create New Photo Ok JPG", func(t *testing.T) {
+		fileName := "../common/cat2.jpg"
+		f, err := os.Open(fileName)
+		require.NoError(t, err)
+
+		params := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+		res, err := beClient.Photos.CreateNewPhoto(params, auth)
+		require.NoError(t, err)
+
+		assert.NotEmpty(t, res.Payload.Data.ID)
+		assert.NotEmpty(t, res.Payload.Data.URL)
+		assert.NotEmpty(t, res.Payload.Data.FileName)
+
+		// cleanup
+		_, err = beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth)
+		require.NoError(t, err)
+		f.Close()
+	})
+
+	t.Run("Create New Photo failed: Wrong file format. File should be jpg or jpeg", func(t *testing.T) {
+		fileName := "../common/cat3.png"
+		f, err := os.Open(fileName)
+		require.NoError(t, err)
+
+		params := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+		_, err = beClient.Photos.CreateNewPhoto(params, auth)
+		require.Error(t, err)
+
+		var phErr *photos.CreateNewPhotoBadRequest
+		require.True(t, errors.As(err, &phErr))
+
+		wantMessage := "Wrong file format. File should be jpg or jpeg"
+		require.NotNil(t, phErr.Payload.Data)
+		assert.Equal(t, wantMessage, phErr.Payload.Data.Message)
+		f.Close()
+	})
+
+	t.Run("Create New Photo failed: empty file - swagger 'multipart: NextPart: EOF'", func(t *testing.T) {
+		emptyFile, err := os.Create("empty.jpeg")
+		require.NoError(t, err)
+
+		params := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(emptyFile)
+		_, err = beClient.Photos.CreateNewPhoto(params, auth)
+		require.Error(t, err)
+
+		var phErr *photos.CreateNewPhotoBadRequest
+		require.True(t, errors.As(err, &phErr))
+		assert.Nil(t, phErr.Payload.Data)
+
+		emptyFile.Close()
+		require.NoError(t, os.Remove("empty.jpeg"))
+	})
+
+	t.Run("Create New Photo failed: access failed", func(t *testing.T) {
+		fileName := "../common/cat2.jpg"
+		f, err := os.Open(fileName)
+		require.NoError(t, err)
+
+		token := common.TokenNotExist
+		_, gotErr := beClient.Photos.CreateNewPhoto(photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f),
+			common.AuthInfoFunc(&token))
+		require.Error(t, gotErr)
+
+		wantErr := photos.NewCreateNewPhotoDefault(http.StatusInternalServerError)
+		wantErr.Payload = &models.Error{Data: nil}
+		assert.Equal(t, wantErr, gotErr)
+		f.Close()
 	})
 }
 
-func writable(path string) bool {
-	return unix.Access(path, unix.W_OK) == nil
-}
-func generateImageBytes() ([]byte, error) {
-	buf := new(bytes.Buffer)
-	err := jpeg.Encode(buf, image.Rect(0, 0, 100, 100), nil)
-	if err != nil {
-		return nil, err
+func TestIntegration_DeletePhoto(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
 	}
-	return buf.Bytes(), nil
+
+	ctx := context.Background()
+	beClient := common.SetupClient()
+
+	fileName := "../common/cat.jpeg"
+	f, err := os.Open(fileName)
+	require.NoError(t, err)
+
+	createParams := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+	res, err := beClient.Photos.CreateNewPhoto(createParams, auth)
+	require.NoError(t, err)
+
+	t.Run("Delete Photo failed: access failed", func(t *testing.T) {
+		token := common.TokenNotExist
+		_, gotErr := beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID),
+			common.AuthInfoFunc(&token))
+
+		require.Error(t, gotErr)
+
+		wantErr := photos.NewDeletePhotoDefault(http.StatusInternalServerError)
+		wantErr.Payload = &models.Error{Data: nil}
+		assert.Equal(t, wantErr, gotErr)
+	})
+
+	t.Run("Delete Photo failed: swagger validation, photoID not provided", func(t *testing.T) {
+		_, gotErr := beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx), auth)
+		require.Error(t, gotErr)
+
+		wantErr := photos.NewDeletePhotoDefault(http.StatusUnprocessableEntity)
+		wantErr.Payload = &models.Error{Data: nil}
+		assert.Equal(t, wantErr, gotErr)
+	})
+
+	t.Run("Delete Photo Ok", func(t *testing.T) {
+		result, err := beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth)
+		require.NoError(t, err)
+
+		got := result.Payload
+		want := "Photo deleted"
+		assert.Equal(t, want, got)
+	})
+
+	t.Run("Delete Photo failed: trying to delete again the same photo, photo not found", func(t *testing.T) {
+		_, err = beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth)
+		require.Error(t, err)
+
+		var phErr *photos.DeletePhotoDefault
+		ok := errors.As(err, &phErr)
+		require.True(t, ok)
+
+		assert.Contains(t, phErr.Payload.Data.Message, "photo not found")
+	})
+
+	f.Close()
 }
 
-func createNonEmptyFile(name string, content []byte) error {
-	f, err := os.Create(name)
-	if err != nil {
-		return err
+func TestIntegration_PhotosDownload(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
 	}
-	_, err = f.Write(content)
-	if err != nil {
-		if err := os.Remove(name); err != nil {
-			return err
-		}
-		return err
+
+	ctx := context.Background()
+	beClient := common.SetupClient()
+
+	t.Run("Download Photo Ok JPEG", func(t *testing.T) {
+		fileName := "../common/cat.jpeg"
+		f, err := os.Open(fileName)
+		require.NoError(t, err)
+
+		createParams := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+		res, err := beClient.Photos.CreateNewPhoto(createParams, auth)
+		require.NoError(t, err)
+
+		result, err := beClient.Photos.DownloadPhoto(photos.NewDownloadPhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth, io.Discard)
+		require.NoError(t, err)
+
+		assert.Equal(t, io.Discard, result.Payload)
+		// cleanup
+		_, err = beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth)
+		require.NoError(t, err)
+		f.Close()
+	})
+
+	t.Run("Download Photo Ok JPG", func(t *testing.T) {
+		fileName := "../common/cat2.jpg"
+		f, err := os.Open(fileName)
+		require.NoError(t, err)
+
+		createParams := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+		res, err := beClient.Photos.CreateNewPhoto(createParams, auth)
+		require.NoError(t, err)
+
+		result, err := beClient.Photos.DownloadPhoto(photos.NewDownloadPhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth, io.Discard)
+		require.NoError(t, err)
+
+		assert.Equal(t, io.Discard, result.Payload)
+		// cleanup
+		_, err = beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth)
+		require.NoError(t, err)
+		f.Close()
+	})
+
+	t.Run("Download Photo failed: access failed", func(t *testing.T) {
+		fileName := "../common/cat.jpeg"
+		f, err := os.Open(fileName)
+		require.NoError(t, err)
+
+		createParams := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+		res, err := beClient.Photos.CreateNewPhoto(createParams, auth)
+		require.NoError(t, err)
+
+		token := common.TokenNotExist
+		_, gotErr := beClient.Photos.DownloadPhoto(photos.NewDownloadPhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID),
+			common.AuthInfoFunc(&token), io.Discard)
+		require.Error(t, gotErr)
+
+		wantErr := photos.NewDownloadPhotoDefault(http.StatusInternalServerError)
+		wantErr.Payload = &models.Error{Data: nil}
+		assert.Equal(t, wantErr, gotErr)
+		f.Close()
+	})
+
+	t.Run("Download Photo failed: photoID not provided", func(t *testing.T) {
+		_, err := beClient.Photos.DownloadPhoto(photos.NewDownloadPhotoParamsWithContext(ctx), auth, io.Discard)
+		require.Error(t, err)
+
+		var phErr *photos.DownloadPhotoDefault
+		require.True(t, errors.As(err, &phErr))
+
+		assert.Equal(t, http.StatusInternalServerError, phErr.Code())
+		assert.Contains(t, phErr.Payload.Data.Message, "photo not found")
+	})
+}
+
+func TestIntegration_PhotoGet(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
 	}
-	if err := f.Close(); err != nil {
-		return err
-	}
-	return nil
+
+	ctx := context.Background()
+	beClient := common.SetupClient()
+
+	t.Run("Get Photo Ok JPEG", func(t *testing.T) {
+		fileName := "../common/cat.jpeg"
+		f, err := os.Open(fileName)
+		require.NoError(t, err)
+
+		createParams := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+		res, err := beClient.Photos.CreateNewPhoto(createParams, auth)
+		require.NoError(t, err)
+
+		result, err := beClient.Photos.GetPhoto(photos.NewGetPhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth, io.Discard)
+		require.NoError(t, err)
+
+		assert.Equal(t, io.Discard, result.Payload)
+		// cleanup
+		_, err = beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth)
+		require.NoError(t, err)
+		f.Close()
+	})
+
+	t.Run("Get Photo Ok JPG", func(t *testing.T) {
+		fileName := "../common/cat2.jpg"
+		f, err := os.Open(fileName)
+		require.NoError(t, err)
+
+		createParams := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+		res, err := beClient.Photos.CreateNewPhoto(createParams, auth)
+		require.NoError(t, err)
+
+		result, err := beClient.Photos.GetPhoto(photos.NewGetPhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth, io.Discard)
+		require.NoError(t, err)
+
+		assert.Equal(t, io.Discard, result.Payload)
+		// cleanup
+		_, err = beClient.Photos.DeletePhoto(photos.NewDeletePhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), auth)
+		require.NoError(t, err)
+		f.Close()
+	})
+
+	t.Run("Get Photo failed: access failed", func(t *testing.T) {
+		fileName := "../common/cat.jpeg"
+		f, err := os.Open(fileName)
+		require.NoError(t, err)
+
+		createParams := photos.NewCreateNewPhotoParamsWithContext(ctx).WithFile(f)
+		res, err := beClient.Photos.CreateNewPhoto(createParams, auth)
+		require.NoError(t, err)
+		token := common.TokenNotExist
+		_, gotErr := beClient.Photos.GetPhoto(photos.NewGetPhotoParamsWithContext(ctx).WithPhotoID(res.Payload.Data.ID), common.AuthInfoFunc(&token), io.Discard)
+		require.Error(t, gotErr)
+
+		wantErr := photos.NewGetPhotoDefault(http.StatusInternalServerError)
+		wantErr.Payload = &models.Error{Data: nil}
+		assert.Equal(t, wantErr, gotErr)
+		f.Close()
+	})
+
+	t.Run("Get Photo failed: swagger validation, photoID not provided", func(t *testing.T) {
+		_, gotErr := beClient.Photos.GetPhoto(photos.NewGetPhotoParamsWithContext(ctx), auth, io.Discard)
+		require.Error(t, gotErr)
+
+		wantErr := photos.NewGetPhotoDefault(http.StatusUnprocessableEntity)
+		wantErr.Payload = &models.Error{Data: nil}
+		assert.Equal(t, wantErr, gotErr)
+	})
 }
