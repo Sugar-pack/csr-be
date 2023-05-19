b63b96fe2d0b5825807edbbb0a3502138b890829
Dmitrii_Sushkevich <dmitrii_sushkevich@epam.com>
2023-05-19 14:31:33 +0300

    fix test

--- internal/db/connection_test.go
+++ internal/db/connection_test.go
@@ -1,12 +1,13 @@
 package db
 
 import (
+	"git.epam.com/epm-lstr/epm-lstr-lc/be/internal/config"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetDB(t *testing.T) {
-	_, _, err := GetDB("host=123")
+	_, _, err := GetDB(config.DB{Host: "123"})
 	require.ErrorContains(t, err, "failed to ping sql connection: failed to connect")
 }
